e38547b29a5e2e23ee07e03b717f4d0aa9946fd7
Benaiah <alumonabenaiah71@gmail.com>
2024-01-22 20:52:26 +0100

    updated

---
+++ src/components/blurBgRadial.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+interface BlurBgRadialProp {
+  className?: React.ComponentProps<"div">["className"];
+}
+
+function BlurBgRadial({ className }: BlurBgRadialProp) {
+  return (
+    <div
+      className={twMerge(
+        `rounded-[50%] blur-[250px] w-[35%] h-[55%] opacity-70 z-[10] fixed transform bg-dark-300/[6%]`,
+        className
+      )}
+    ></div>
+  );
+}
+
+export default BlurBgRadial;
