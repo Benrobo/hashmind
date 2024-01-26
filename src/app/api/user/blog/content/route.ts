9c8f809a522783f15f7b1d88c499cbccbe280abd
Benaiah <alumonabenaiah71@gmail.com>
2024-01-26 20:49:05 +0100

    updated

---
+++ src/app/api/user/blog/content/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest } from "next/server";
+import { isAuthenticated } from "../../../middlewares/auth";
+import CatchError from "../../../utils/_error";
+import UserController from "@api/controller/user.controller";
+
+const userController = new UserController();
+
+export const GET = CatchError(
+  isAuthenticated(
+    async (req: NextRequest) => await userController.getContents(req)
+  )
+);
+
+export const POST = CatchError(
+  isAuthenticated(
+    async (req: NextRequest) => await userController.removeContentMetadata(req)
+  )
+);
