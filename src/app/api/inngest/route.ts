89d8e533c0c45c635485f9f00bec5b562c68487a
Benaiah <alumonabenaiah71@gmail.com>
2024-01-29 00:11:31 +0100

    updated

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,7 +2,6 @@ import { serve } from "inngest/next";
 import { inngest } from "@/app/api/config/inngest_client";
 import {
   inngest_article_coverimage_generation_function,
-  inngest_article_metadata_creation_function,
   inngest_hashmind_main_function,
   inngest_article_content_generation_function,
   inngest_publish_article_function,
@@ -16,7 +15,6 @@ export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     inngest_hashmind_main_function,
-    inngest_article_metadata_creation_function,
     inngest_article_coverimage_generation_function,
     inngest_article_content_generation_function,
     inngest_publish_article_function,
