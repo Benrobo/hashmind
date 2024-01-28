3601e582ea66bd3e047b106a93508557d42d6f4b
Benaiah <alumonabenaiah71@gmail.com>
2024-01-29 00:11:26 +0100

    updated

---
+++ src/app/api/data/test_data.ts
@@ -0,0 +1,35 @@
+// this contains the test data for all services / functions
+// Just a hack i USE to prevent further costs usage.
+
+export const userActionTestData = {
+  // error: null,
+  // action: "CREATE_BLOG",
+  // title:
+  //   "Why Artificial Intelligence is the future of humanity and how it won't change the world",
+  // emoji: "🤖",
+  // subtitle: "Exploring the Promise and Limitations of AI",
+  // aiMsg: null,
+  // keywords:
+  //   "Artificial Intelligence, future, humanity, limitations, promise",
+
+  // update
+  functions: ["identify_action", "identify_update_blog_action"],
+  error: null,
+  action: "UPDATE_BLOG",
+  // title: null,
+  title: "Why Artificial Intelligence is the future of humanity",
+  emoji: null,
+  subtitle: null,
+  keywords: null,
+  aiMsg: null,
+  //   generate a new title
+  updateTitle: "Artificial Intelligence, the future of humanity.",
+  // updateTitle: null,
+  // updateContent: "limitations of AI, promise of AI",
+  updateContent: null,
+  updateSubtitle: null,
+  // updateSubtitle: "Discussing the impact of AI on society and human life",
+  //   updateCoverImage: null,
+  updateCoverImage: "Utopian future, AI, harmony",
+  updateContentNotation: "ADD",
+};
