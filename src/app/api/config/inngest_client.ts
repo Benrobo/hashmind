60ca2858ff4684b188adbf9cedbaf47e6faa5e37
Benaiah <alumonabenaiah71@gmail.com>
2024-01-29 00:11:08 +0100

    Add newTitle field to HashmindMainFunc

--- src/app/api/config/inngest_client.ts
+++ src/app/api/config/inngest_client.ts
@@ -57,6 +57,7 @@ type HashmindMainFunc = {
     // for updating article content
     updateContentNotation?: updateBlogContentNotationType;
     request?: string;
+    newTitle?: string;
   };
 };
 
