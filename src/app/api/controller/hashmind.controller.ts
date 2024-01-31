98b515ce708323e8a42b851905c69fa84da963d6
Benaiah <alumonabenaiah71@gmail.com>
2024-01-31 23:39:04 +0100

    Refactor hashmind.controller.ts

--- src/app/api/controller/hashmind.controller.ts
+++ src/app/api/controller/hashmind.controller.ts
@@ -1,24 +1,7 @@
 import { NextRequest } from "next/server";
 import ZodValidation from "../utils/zodValidation";
 import { handleUserRequestSchema } from "../utils/schema_validation";
-import sendResponse from "../utils/sendResponse";
-import {
-  HashmindAIResponseAction,
-  RESPONSE_CODE,
-  updateBlogContentNotationType,
-} from "@/types";
 import speechToText from "../services/stt.service";
-import identifyAction, {
-  IdentifyActionRespType,
-} from "../functions/identifyAction";
-import HttpException from "../utils/exception";
-import textToSpeech from "../services/tts.service";
-import { actionsVariants, supportedActions } from "../data/ai/function";
-import { inngest } from "../config/inngest_client";
-import prisma from "@/prisma/prisma";
-import { nanoid } from "nanoid";
-import { transcriptTestData, userActionTestData } from "../data/test_data";
-import redis from "../config/redis";
 import processUserRequests from "../services/processReq.service";
 
 type ReqUserObj = {
