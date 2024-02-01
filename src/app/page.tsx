0f884c29156915e302ad0fb0a5475dc41004660d
Benaiah <alumonabenaiah71@gmail.com>
2024-02-01 04:19:12 +0100

    Add SEO component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@ import Footer from "@/components/footer";
 import Hero from "@/components/landing/hero";
 import Overview from "@/components/landing/overview";
 import HomeTopBar from "@/components/navbar";
-import Image from "next/image";
+import Seo from "@/components/seo";
 
 export default function Home() {
   return (
     <main className=" bg-dark-100">
+      <Seo />
       <HomeTopBar />
       <Hero />
       <Overview />
